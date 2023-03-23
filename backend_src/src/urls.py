3a7230ce82a7f18b893caf67dde3d0c2ef210de5
Petercode <phamphuoc19032001@gmail.com>
2023-03-23 17:12:49 +0700

    cmt

--- backend_src/src/urls.py
+++ backend_src/src/urls.py
@@ -4,6 +4,7 @@ from src import views
 # http://127.0.0.1:8000/
 urlpatterns = [
     path("all_exams/", views.query_all_exams_api, name="allExams"),
+    path("all_exams/<int:event_id>", views.query_exam_by_id, name="queryExamById"),
     path("exams/", views.query_exams_by_userid, name="queryExamByUserId"),
     path("posts/", views.test_api, name="postsALL"),
     path("posts/<int:event_id>", views.delete_api, name="deleteOne"),
